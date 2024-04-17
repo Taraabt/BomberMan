bca87a9ccd645736a3c6bfd085a4472cf1d60a0d
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-04-17 12:34:27 +0200

    Add Project

---
+++ BomberManRemake/Assets/Script/Player/Input Controller.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.WSA;
+
+public class InputController : MonoBehaviour
+{
+    Input input;
+    Vector2 move;
+    [SerializeField]float moveTime;
+    [SerializeField] GameObject bomb;     
+    bool canMove=true;
+
+
+    private void Awake()
+    {
+        input = new Input();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Movement.performed += MovePerformed;
+        input.Player.Movement.canceled += MoveCanceled;
+        input.Player.Bomb.performed += Launch;
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Movement.performed -= MovePerformed;
+        input.Player.Movement.canceled -= MoveCanceled;
+        input.Player.Bomb.performed += Launch;
+    }
+
+    private void MovePerformed(InputAction.CallbackContext value)
+    {
+        move = value.ReadValue<Vector2>();
+        transform.position += new Vector3(move.x, 0, move.y);
+
+    }
+    private void MoveCanceled(InputAction.CallbackContext value)
+    {
+        move = Vector2.zero;
+    }
+
+    IEnumerator Move() {
+        canMove = false;
+        yield return new WaitForSeconds(moveTime);
+        transform.position += new Vector3(move.x, 0, move.y);
+        canMove = true;
+    }
+
+    private void Update()
+    {
+        if (input.Player.Movement.IsPressed()&&canMove)
+        {
+            StartCoroutine(Move());
+        }
+    }
+
+    private void Launch(InputAction.CallbackContext value)
+    {
+        Instantiate(bomb,transform.position,Quaternion.identity);
+    }
+
+
+
+}
